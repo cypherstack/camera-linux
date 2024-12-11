3557914356ebcb5f489b470de4f762682d3e02bd
julian <julian@cypherstack.com>
2024-12-11 17:42:47 -0600

    fix: free memory after use

--- lib/camera_linux.dart
+++ lib/camera_linux.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:ffi';
 import 'dart:typed_data';
-import 'camera_linux_bindings_generated.dart';
+
 import 'package:ffi/ffi.dart';
+
+import 'camera_linux_bindings_generated.dart';
 
 class CameraLinux {
   late CameraLinuxBindings _bindings;
@@ -24,7 +26,7 @@ class CameraLinux {
   }
 
   // Convert Frame Into Base64
-  String uint8ListToBase64Url(Uint8List data) {
+  String _uint8ListToBase64Url(Uint8List data) {
     String base64String = base64Encode(data);
 
     String base64Url = base64String
@@ -39,14 +41,24 @@ class CameraLinux {
   // Capture The Frame
   Future<String> captureImage() async {
     final lengthPtr = calloc<Int>();
-    Pointer<Uint8> framePointer = _bindings.getLatestFrameBytes(lengthPtr);
-    final latestFrame = getLatestFrameData(framePointer, lengthPtr.value);
-    final base64Image = uint8ListToBase64Url(latestFrame);
-    return base64Image;
+    Pointer<Uint8>? latestFramePtr;
+    try {
+      latestFramePtr = _bindings.getLatestFrameBytes(lengthPtr);
+      final latestFrame = _getLatestFrameData(latestFramePtr, lengthPtr.value);
+      final base64Image = _uint8ListToBase64Url(latestFrame);
+      return base64Image;
+    } catch (_) {
+      rethrow;
+    } finally {
+      calloc.free(lengthPtr);
+      if (latestFramePtr != null) {
+        calloc.free(latestFramePtr);
+      }
+    }
   }
 
   // Get The Latest Frame
-  Uint8List getLatestFrameData(Pointer<Uint8> framePointer, frameSize) {
+  Uint8List _getLatestFrameData(Pointer<Uint8> framePointer, frameSize) {
     List<int> frameList = framePointer.asTypedList(frameSize);
     return Uint8List.fromList(frameList);
   }
